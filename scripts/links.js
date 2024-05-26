5c1030caa74fa8be7cbc203b769a8bf41be2e7c9
srejas <srejas@outlook.com>
2024-05-25 21:23:55 -0600

    Home page weather and links updated to dynamic

--- scripts/links.js
+++ scripts/links.js
@@ -1,13 +1,14 @@
 const baseUrl = "https://srejas.github.io/wdd230/";
-const linksUrl = "https://srejas.github.io/wdd230/data/links.json"
+const linksUrl = "https://srejas.github.io/wdd230/data/links.json";
+
+const courseCard = document.querySelector(".card");
 
 async function getCourseLinks() {
     try {
         const response = await fetch(linksUrl);
         if (response.ok) {
             const data = await response.json();
-            // displayResults(data);
-            console.log(data);
+            displayLinks(data.weeks);
         }
         else {
             throw Error(await response.text());
@@ -16,3 +17,35 @@ async function getCourseLinks() {
         console.log(error);
     }
 }
+
+function displayLinks(weeks) {
+    const courseList = document.createElement('ul');
+
+    weeks.forEach((week) => {
+        const listItem = document.createElement('li');
+
+        listItem.textContent = `${week.week}: `
+
+        let finalLink = week.links.length - 1;
+
+        week.links.forEach((link) => {
+            const newLink = document.createElement('a');
+
+            newLink.textContent = `${link.title}`;
+            newLink.setAttribute('href', `${link.url}`);
+
+            listItem.appendChild(newLink);
+
+            if (link !== week.links[finalLink]) {
+                const newSpan = document.createElement('span');
+                newSpan.textContent = `|`
+                listItem.appendChild(newSpan);
+            }
+        })
+
+        courseList.appendChild(listItem);
+        courseCard.appendChild(courseList);
+    });
+}
+
+getCourseLinks();
